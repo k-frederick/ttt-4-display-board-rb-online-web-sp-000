ff36db36e960b693b09149b45fe22ce5058bb3e3
Kristina Frederick <kristinafrederick@icloud.com>
2020-03-27 21:49:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-puts " o | x |   "
+puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 puts "-----------"
