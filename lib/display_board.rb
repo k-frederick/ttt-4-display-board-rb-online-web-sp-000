004d5223040c4e57f18922734d8510292231b2a6
Kristina Frederick <kristinafrederick@icloud.com>
2020-03-27 21:42:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,2 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board(board)
-  row[0] =     |     |
-  row[1] = ---------------
-  row[2] =     |     |
-  row[3] = ---------------
-  row[4] =     |     |
-end
